6882f67bb313c0ab2297e0f61025db10e16dbb08
Jaike Jennison <jjennison@echo360.com>
2017-03-23 11:16:24 -0400

    Create install.bat

---
+++ install.bat
@@ -0,0 +1,13 @@
+CLS
+@ECHO ==============================================
+@ECHO === Classroom Capture Unattended Installer ===
+@ECHO ===        With Teachers Pet alpha         ===
+@ECHO ===           by Jaike Jennison            ===
+@ECHO ==============================================
+@ECHO OFF
+CD %USERPROFILE%
+CD Downloads
+DIR /w/p
+PAUSE
+Echo360ClassroomCaptureForWindows.exe --mode unattended --UninstallPreviousVersions true --debuglevel 4 --pre "C:\ProgramData\Echo360" --prefix "C:\Program Files\Echo360"
+PAUSE
