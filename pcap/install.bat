fd79dd294568c0d6244c4dd607982515031e8b1d
Jaike Jennison <jjennison@echo360.com>
2017-05-01 13:28:31 -0400

    modified pcap to match bitrock

--- pcap/install.bat
+++ pcap/install.bat
@@ -9,5 +9,5 @@ CD %USERPROFILE%
 CD Downloads
 DIR /p
 PAUSE
-Echo360PersonalCaptureInstaller.exe --mode unattended --debugtrace debug.txt --debuglevel 4 --pre "C:\ProgramData\Echo360\PersonalCapture" --prefix "C:\Program Files\Echo360\Personal Capture"
+Echo360PersonalCaptureInstaller.exe --mode unattended --debugtrace debug.txt --debuglevel 4 --pre "C:\ProgramData\Echo360\PersonalCapture" --prefix "C:\Program Files\Echo360\Personal Capture\Client"
 PAUSE
